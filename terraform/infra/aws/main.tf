8d7c7694b297252f5edc97572d2399243316c068
bhumi46 <thisisbn46@gmail.com>
2025-08-22 18:16:10 +0530

    [MOSIP-42509] Updated postgresql-setup.sh
    Signed-off-by: bhumi46 <thisisbn46@gmail.com>

--- terraform/infra/aws/main.tf
+++ terraform/infra/aws/main.tf
@@ -38,4 +38,14 @@ module "aws_infrastructure" {
   K8S_INSTANCE_ROOT_VOLUME_SIZE = var.k8s_instance_root_volume_size
   network_cidr                  = var.network_cidr
   WIREGUARD_CIDR                = var.WIREGUARD_CIDR
+  
+  # PostgreSQL Configuration
+  postgresql_version = var.postgresql_version
+  storage_device     = var.storage_device
+  mount_point        = var.mount_point
+  postgresql_port    = var.postgresql_port
+  
+  # MOSIP Infrastructure Repository Configuration
+  mosip_infra_repo_url = var.mosip_infra_repo_url
+  mosip_infra_branch   = var.mosip_infra_branch
 }
