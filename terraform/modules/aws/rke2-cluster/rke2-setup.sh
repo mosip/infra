f88e50d879a06502b2f9d98b76182943ee11da88
bhumi46 <111699703+bhumi46@users.noreply.github.com>
2025-08-27 16:12:23 +0530

    Disable nounset option in rke2-setup.sh
    Signed-off-by: bhumi46 <111699703+bhumi46@users.noreply.github.com>

--- terraform/modules/aws/rke2-cluster/rke2-setup.sh
+++ terraform/modules/aws/rke2-cluster/rke2-setup.sh
@@ -13,7 +13,7 @@ exec > >(tee -a "$LOG_FILE") 2>&1
 # set commands for error handling.
 set -e
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
-set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+#set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errtrace  # trace ERR through 'time command' and other functions
 set -o pipefail  # trace ERR through pipes
 
