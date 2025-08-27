311f60fed517c128cde2441cc1bc6d733870cbdd
bhumi46 <thisisbn46@gmail.com>
2025-08-27 13:46:32 +0530

    reveret tf cloud init changes
    Signed-off-by: bhumi46 <thisisbn46@gmail.com>

--- terraform/base-infra/azure/outputs.tf
+++
@@ -1,4 +0,0 @@
-# Azure Base Infrastructure Outputs
-# This is a placeholder for Azure base infrastructure outputs
-
-# Add Azure-specific outputs here when implementing
--- terraform/base-infra/gcp/variables.tf
+++
@@ -1,4 +0,0 @@
-# GCP Base Infrastructure Variables
-# This is a placeholder for GCP base infrastructure variables
-
-# Add GCP-specific variables here when implementing
