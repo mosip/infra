f2cf32125425c2c70e4fed3b540d6da84cb115c9
bhumi46 <thisisbn46@gmail.com>
2026-01-23 12:03:13 +0530

    updated keycloak init script
    Signed-off-by: bhumi46 <thisisbn46@gmail.com>

--- Helmsman/hooks/esignet-preinstall-keycloak-init.sh
+++ Helmsman/hooks/esignet-preinstall-keycloak-init.sh
@@ -6,10 +6,25 @@
 NS=esignet
 
 function installing_preinstall_esignet_setup() {
+  echo "creating and adding roles to keycloak pms & mpartner_default_auth clients for ESIGNET"
+  kubectl -n $NS delete secret  --ignore-not-found=true keycloak-client-secrets
+  helm -n $NS delete esignet-keycloak-init || true
   echo "Copying keycloak configmaps and secret to $NS namespace"
   COPY_UTIL=$WORKDIR/utils/copy-cm-and-secrets/copy_cm_func.sh
   $COPY_UTIL configmap keycloak-env-vars keycloak $NS
-  $COPY_UTIL secret keycloak keycloak $NS  
+  $COPY_UTIL secret keycloak keycloak $NS
+
+  # Fetch PMS and MPARTNER secrets from keycloak namespace
+  echo "Checking if PMS & mpartner_default_auth client secrets exist..."
+  export PMS_CLIENT_SECRET_KEY='mosip_pms_client_secret'
+  export PMS_CLIENT_SECRET_VALUE=$(kubectl -n keycloak get secrets keycloak-client-secrets -o jsonpath="{.data.${PMS_CLIENT_SECRET_KEY}}" | base64 -d)
+  export MPARTNER_DEFAULT_AUTH_SECRET_KEY='mpartner_default_auth_secret'
+  export MPARTNER_DEFAULT_AUTH_SECRET_VALUE=$(kubectl -n keycloak get secrets keycloak-client-secrets -o jsonpath="{.data.${MPARTNER_DEFAULT_AUTH_SECRET_KEY}}" | base64 -d)
+
+  echo "PMS_CLIENT_SECRET_KEY: $PMS_CLIENT_SECRET_KEY"
+  echo "MPARTNER_DEFAULT_AUTH_SECRET_KEY: $MPARTNER_DEFAULT_AUTH_SECRET_KEY"
+  # Note: Not printing secret values for security reasons
+
   echo "Pre-install setup complete. Helmsman will now deploy esignet-keycloak-init chart."
   return 0
 }
